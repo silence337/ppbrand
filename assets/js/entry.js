4044fbef62d78e2509f12aea49cb18b000e2f840
silence337 <silence337@naver.com>
2025-05-27 13:39:50 +0900

    Update entry.js

--- assets/js/entry.js
+++ assets/js/entry.js
@@ -276,7 +276,7 @@ class begin {
                 //markers: true,
             },
         });
-        galleryThum.to(galleryList.querySelector('li'), {x:100, y: -150, autoAlpha: 1, ease:'none', stagger: 0.1,});
+        galleryThum.to(galleryList.querySelectorAll('li'), {x:100, y: -150, autoAlpha: 1, ease:'none', stagger: 0.1,});
 
         let galleryPin = gsap.timeline({
             scrollTrigger: {
